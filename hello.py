6bc7a7511d047551660c4c3e2aa0e880f73e9f61
Purvansh Uthaiah B R <uthaiahpurvansh@gmail.com>
2026-04-05 15:41:29 +0530

    "added problem 7 -  Reversing a string"

--- hello.py
+++ hello.py
@@ -1,6 +1,4 @@
-# multiplication table
-n = int(input("Enter the number you want: "))
-i = 0
-for i in range(1,11):
-    print(f"{n} x {i} = {n*i}")
-    i += 1
+#Reversing a string
+text = input("Enter your text: ")
+text2 = text[::-1]
+print(text2)
