cb1b9a11fe1e5a76fc76854cc5626faf51cf6a79
Purvansh Uthaiah B R <uthaiahpurvansh@gmail.com>
2026-04-05 15:45:43 +0530

    "added problem 8 - largest of three numbers without using max()"

--- hello.py
+++ hello.py
@@ -1,4 +1,12 @@
-#Reversing a string
-text = input("Enter your text: ")
-text2 = text[::-1]
-print(text2)
+#largest of the three numbers without using max()
+
+a = int(input("Enter number1: "))
+b = int(input("Enter number2: "))
+c = int(input("Enter number3: "))
+
+if (a>b and a>c):
+    print("Greatest number is",a)
+elif(b>a and b>c):
+    print("Greatest number is",b)
+else:
+    print("Greatest number is",c)
