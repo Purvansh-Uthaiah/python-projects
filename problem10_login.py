cc207b6025f0c633b2904177b4e66b47e53ce4e5
Purvansh Uthaiah B R <uthaiahpurvansh@gmail.com>
2026-04-06 10:58:17 +0530

    "added problem 10 - Login system"

---
+++ problem10_login.py
@@ -0,0 +1,12 @@
+#Login system
+username =  "Purvansh_Uthaiah"
+password = "purva@123"
+
+Username = input("Enter Username: ")
+Password = input("Enter Password: ")
+
+if (Username == username and Password == password ):
+    print("Access to the system granted")
+
+else:
+    print("Wrong credentials ! Try again")
