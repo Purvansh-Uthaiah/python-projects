d3407c8bef9d14a62778f55acd40d5b9fe6cdd4f
Purvansh-Uthaiah <uthaiahpurvansh@gmail.com>
2026-04-06 11:34:14 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,12 +1,2 @@
-#Login system
-username =  "Purvansh_Uthaiah"
-password = "purva@123"
-
-Username = input("Enter Username: ")
-Password = input("Enter Password: ")
-
-if (Username == username and Password == password ):
-    print("Access to the system granted")
-
-else:
-    print("Wrong credentials ! Try again")
+name = input("Enter your name: ")
+print(f"Hello {name} ! Welcome to my GitHub")
